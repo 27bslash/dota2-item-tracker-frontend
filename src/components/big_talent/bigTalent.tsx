13c8d85cc5943822112b12fc79174340d43dd0e1
27bslash <greenpear68@yahoo.co.uk>
2022-11-25 15:12:16 +0000

    changed talents to calculate on client

--- src/components/big_talent/bigTalent.tsx
+++ src/components/big_talent/bigTalent.tsx
@@ -2,30 +2,92 @@ import { useState, useEffect } from 'react';
 import BigTalentTooltip from './bigTalentTooltip';
 import heroSwitcher from '../heroSwitcher';
 
-const BigTalent = (props: { heroName: string, baseApiUrl: string }) => {
-    const [talents, setTalents] = useState<any[]>([])
+const BigTalent = (props: { matchData: any, heroName: string, baseApiUrl: string, heroData: any }) => {
+    const [talents, setTalents] = useState<any>([])
     useEffect(() => {
-        (async () => {
-            const heroName = heroSwitcher(props.heroName)
-            const data = await fetch(`${props.baseApiUrl}files/talent-data/${heroName}`)
-            const json = await data.json()
-            setTalents(json)
-            console.log(json)
-        })()
-    }, [])
+        // (async () => {
+        //     const heroName = heroSwitcher(props.heroName)
+        //     const data = await fetch(`${props.baseApiUrl}files/talent-data/${heroName}`)
+        //     const json = await data.json()
+        //     setTalents(json)
+        // })()
+        // countTalents()
+        countTalents()
+
+    }, [props.matchData])
+    const countTalents = () => {
+        const talentCount: any = {}
+        // initialise object 
+        for (let k in props.heroData['talents']) {
+            const talent = props.heroData['talents'][k]
+            let lvl = 0
+            if (talent['slot'] === 0 || talent['slot'] === 1) {
+                lvl = 10
+            }
+            else if (talent['slot'] === 2 || talent['slot'] === 3) {
+                lvl = 15
+            }
+            else if (talent['slot'] === 4 || talent['slot'] === 5) {
+                lvl = 20
+            }
+            else if (talent['slot'] === 6 || talent['slot'] === 7) {
+                lvl = 25
+            }
+            talentCount[talent['name_loc']] = { count: 0, slot: talent['slot'], total_picks: 0, level: lvl }
+
+        }
+        for (let match of props.matchData) {
+            for (let ability of match['abilities']) {
+                if (ability['type'] === 'talent') {
+                    const k = ability['key']
+                    const slot = ability['slot']
+                    const count = talentCount[k]['count']
+                    talentCount[k] = { count: count + 1, slot: slot, total_picks: talentCount[k]['total_picks'], level: talentCount[k]['level'] }
+                }
+            }
+
+        }
+        for (let k in talentCount) {
+            const slot = talentCount[k]['slot']
+            if (talentCount[k]['slot'] % 2 === 0) {
+                const found = Object.keys(talentCount).find(key => {
+                    return talentCount[key]['slot'] === slot + 1
+                });
+                if (found) {
+                    talentCount[found]['total_picks'] = talentCount[k]['count'] + talentCount[found]['count']
+                    talentCount[k]['total_picks'] = talentCount[k]['count'] + talentCount[found]['count']
+                }
+            }
+        }
+        const sortO = (a: any, b: any) => {
+            return talentCount[a]['slot'] - talentCount[b]['slot']
+        }
+        const ordered = Object.keys(talentCount).sort((a, b) => sortO(a, b)).reduce(
+            (obj: any, key: string) => {
+                obj[key] = talentCount[key];
+                return obj;
+            },
+            {}
+        );
+        setTalents(ordered)
+    }
+
     return (
         <div className="talent-wrapper">
-            <BigTalentTooltip talents={talents}>
-                <div className="big-talent talents" >
-                    {talents.reverse().map((x, i) => {
-                        const side = x['slot'] % 2 !== 0 ? 'l-talent' : 'r-talent'
-                        const lvl = x['level']
-                        if (x['talent_count'] * 2 >= x['total_pick_count']) {
-                            return <div key={i} className={'lvl' + lvl + ' ' + side}></div>
-                        }
-                    })}
-                </div>
-            </BigTalentTooltip >
+            {talents &&
+                <BigTalentTooltip talents={talents}>
+                    <div className="big-talent talents" >
+                        {Object.entries(talents).reverse().map((x, i) => {
+                            const v: any = x[1]
+                            const side = v['slot'] % 2 === 0 ? 'l-talent' : 'r-talent'
+                            if (v['count'] * 2 >= v['total_picks'] && v['count']) {
+                                return <div key={i} className={'lvl' + v['level'] + ' ' + side}></div>
+                            }
+
+                        })}
+                    </div>
+                </BigTalentTooltip >
+            }
         </div >
     )
 }
--- src/components/big_talent/bigTalentTooltip.tsx
+++ src/components/big_talent/bigTalentTooltip.tsx
@@ -1,29 +1,30 @@
-import  { useMemo } from 'react';
+import { useMemo } from 'react';
 import { useState, Fragment } from 'react';
 
 const BigTalentTooltip = (props: any) => {
     const [open, setOpen] = useState(false)
     const comp = () => {
-        console.log('momo')
+        const keys = Object.keys(props.talents)
         let j = 0
         const rows = []
         for (let i = 0; i < 4; i++) {
-            rows[i] = [props.talents[j], props.talents[j + 1]]
+            rows[i] = [{ [keys[j]]: props.talents[keys[j]] }, { [keys[j + 1]]: props.talents[keys[j + 1]] }]
             j += 2
         }
-        return rows
+        return rows.reverse()
     }
-    const memo = useMemo(() => comp(), [props.talents])
 
+    const memo = useMemo(() => comp(), [props.talents])
     return (
         <div className="toltip" onMouseEnter={() => setOpen(true)}
             onMouseLeave={() => setOpen(false)}>
             {props.children}
-            {open && props.talents.length > 0 &&
+            {open && memo.length > 0 &&
                 <div className="tooltip" id='talents' style={{ marginRight: '105px', marginTop: '11px' }}>
                     {memo.map((x: any, i: number) => {
                         // console.log('ros', props.talents)
-                        const level = x[0]['level']
+                        const k = Object.keys(x[0])[0]
+                        const level = x[0][k]['level']
                         return (
                             <Fragment key={i}>
                                 <TalentRow side='talent-left' talent={x[0]}></TalentRow>
@@ -43,11 +44,12 @@ const BigTalentTooltip = (props: any) => {
     )
 }
 const TalentRow = (props: any) => {
-    const perc = props.talent.talent_count / props.talent.total_pick_count * 100
+    const k = Object.keys(props.talent)[0]
+    const perc = props.talent[k]['count'] / props.talent[k].total_picks * 100 || 0
     return (
         <div className={props.side}>
             <p className='talent-text'>
-                {props.talent.key}
+                {k}
             </p>
             <div className="talent-bar" style={{ width: perc + '%' }}></div>
             <p>{Math.round(perc)}%</p>
