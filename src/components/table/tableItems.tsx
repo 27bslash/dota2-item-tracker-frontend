7725b5509ebd993285a03c8ea760a565c36a447c
27bslash <greenpear68@yahoo.co.uk>
2022-11-28 18:38:47 +0000

    added zero before number if under 10

--- src/components/table/tableItems.tsx
+++ src/components/table/tableItems.tsx
@@ -51,6 +51,7 @@ const humanReadableTime = (time: number | string) => {
         const hours = +spit[0].replace(/^0/, '')
         minutes = +spit[1].replace(/^0/, '') + (hours * 60)
         secs = spit[2].replace(/^0/, '')
+        secs = +secs >= 10 ? secs : `0${secs}`
     } else {
         minutes = Math.floor(time / 60)
         secs = (time % 60) >= 10 ? (time % 60) : `0${(time % 60)}`
