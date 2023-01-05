01adf0c7dbf0e715e1ff826106ff8ddbcd1971b5
27bslash <greenpear68@yahoo.co.uk>
2023-01-05 20:56:21 +0000

    fixed tooltip description missing return

--- src/components/tooltip/tooltipDescription.tsx
+++ src/components/tooltip/tooltipDescription.tsx
@@ -2,7 +2,6 @@ import CdMc from "./cdmc";
 
 const TooltipDescription = (props: any) => {
 
-
     return (
         <div className="tooltip-description">
             {
@@ -11,17 +10,29 @@ const TooltipDescription = (props: any) => {
                     const header = x.match(/.*(?=--)/)
                     const text = x.match(/(?=--).*/)
                     let highlightedText = ''
-                    let active = x.match('Active:')
-                    let passive = x.match('Passive:')
-                    let use = x.match('Use:')
-                    let toggle = x.match('Toggle:')
+                    const active = x.match('Active:')
+                    const passive = x.match('Passive:')
+                    const use = x.match('Use:')
+                    const toggle = x.match('Toggle:')
 
                     if (header && text) {
+                        console.log(header[0])
+                        console.log(text[0])
+
+                        // let activeText = highlight_numbers(x.match(/.*<h1>Active:.*/g));
+                        // let toggleText = highlight_numbers(x.match(/.*<h1>Toggle:.*/g));
+                        // let passiveText = highlight_numbers(x.match(/.*<h1>Passive:.*/g));
+                        // let useText = highlight_numbers(x.match(/.*<h1>Use:.*/g));
                         const preProcessedText = text[0].replace(/(\.)(?=[A-Z])/, '$1<br></br>')
                         highlightedText = highlight_numbers(preProcessedText.replace('--', '').trim())
-                            {active && highlightedText &&
+                        console.log(highlightedText)
+                        // const activeHeader = x.replace(/<h1>(.*)<\/h1>.*/g, "$1");
+                        // const activeTxt = x.replace(/.*<\/h1>(.*)/g, "$1");
+                        // const activeDescText = highlight_numbers(activeTxt)
+                    }
+                    return (
                         <div key={i}>
-                            {activeText &&
+                            {active && highlightedText &&
                                 <div className="active">
                                     <div className="active-header">
                                         <h3 className='tooltip-text-highlight'>{header}</h3>
