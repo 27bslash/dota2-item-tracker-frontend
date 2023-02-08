223d386527f3c7f7bceddc6a55f9650c5286acba
27bslash <greenpear68@yahoo.co.uk>
2023-02-08 16:02:02 +0000

    restructured starting items

--- src/components/HeroBuilds/itemBuild/startingItems/startingItems.tsx
+++ src/components/HeroBuilds/itemBuild/startingItems/startingItems.tsx
@@ -1,7 +1,8 @@
 import TableItem from "../../../table/tableItem"
+import countStartingItems from "./startingItemsFilter"
 
 const StartingItems = (props: any) => {
-    const items = countItems(props.data)
+    const items = countStartingItems(props.data)
     return (
         <div className="most-used-starting-items">
             {items.map((arr, idx) => {
@@ -19,36 +20,5 @@ const StartingItems = (props: any) => {
         </div>
     )
 }
-const countItems = (data: any) => {
-    const wards = [
-        "ward_observer",
-        "ward_sentry",
-        "smoke_of_deceit"
-    ]
 
-    let itemCount: any = {}
-    const comboCount: any = {}
-    for (let match of data) {
-        const key = match['starting_items'].filter((x: { key: string }) => {
-            if (!wards.includes(x['key']))
-                return x['key']
-        }).map((x: { key: string }) => x.key).sort().join('__')
-        if (key.includes('tango')) {
-            // console.log('key', key)
-            comboCount[key] = (comboCount[key] + 1) || 1
-        }
-        const amounts: any = {}
-        // for (let item of match['starting_items']) {
-        //     const charges = item['charges'] || 1
-        //     amounts[item['key']] = (amounts[item['key']] + charges) || 1;
-        // }
-        // // console.log(match['id'], amounts, match['starting_items'])
-        // for (let item of match['starting_items']) {
-        //     const k = `${item['key']}_${amounts[item['key']]}`
-        //     itemCount[k] = (itemCount[k] + 1) || 1;
-        // }
-    }
-    const count = Object.entries(comboCount).sort((a: any, b: any) => b[1] - a[1]).slice(0, 2)
-    return count
-}
 export default StartingItems
---
+++ src/components/HeroBuilds/itemBuild/startingItems/startingItemsFilter.tsx
@@ -0,0 +1,33 @@
+const countStartingItems = (data: any) => {
+    const wards = [
+        "ward_observer",
+        "ward_sentry",
+        "smoke_of_deceit"
+    ]
+
+    let itemCount: any = {}
+    const comboCount: any = {}
+    for (let match of data) {
+        const key = match['starting_items'].filter((x: { key: string }) => {
+            if (!wards.includes(x['key']))
+                return x['key']
+        }).map((x: { key: string }) => x.key).sort().join('__')
+        if (key.includes('tango')) {
+            // console.log('key', key)
+            comboCount[key] = (comboCount[key] + 1) || 1
+        }
+        const amounts: any = {}
+        // for (let item of match['starting_items']) {
+        //     const charges = item['charges'] || 1
+        //     amounts[item['key']] = (amounts[item['key']] + charges) || 1;
+        // }
+        // // console.log(match['id'], amounts, match['starting_items'])
+        // for (let item of match['starting_items']) {
+        //     const k = `${item['key']}_${amounts[item['key']]}`
+        //     itemCount[k] = (itemCount[k] + 1) || 1;
+        // }
+    }
+    const count = Object.entries(comboCount).sort((a: any, b: any) => b[1] - a[1]).slice(0, 2)
+    return count
+}
+export default countStartingItems
