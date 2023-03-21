af5c331458f59999754f1dae49ee739795c7c39e
27bslash <greenpear68@yahoo.co.uk>
2023-03-21 15:42:23 +0000

    fixed tooltip breaking on player pages

--- src/components/tooltip/abilityTooltip.tsx
+++ src/components/tooltip/abilityTooltip.tsx
@@ -10,20 +10,19 @@ import AbilityAttributes from './abilityAttributes';
 const AbilityTooltip = (props: any) => {
     let id = props.ability.id
     const [open, setOpen] = useState(false)
-    const [ability, setAbility] = useState<any>([])
-    console.log(props.ability)
+    const [ability, setAbility] = useState<any>()
     useEffect(() => {
-        // console.log(props.heroData, props.heroName)
         for (let hero of props.heroData) {
-            if (hero[props.heroName] && id) {
-                console.log(hero, props.ability)
+            if (hero[props.heroName] && id && !ability) {
                 setAbility(hero[props.heroName].abilities[+id])
-            } else {
+                break
+            } else if (!ability) {
                 const abilities = hero[props.heroName].abilities
                 const k = Object.keys(props.ability)[0]
                 const a = Object.entries(abilities).find((x: any) => x[1].name === k)
                 if (a)
                     setAbility(a[1])
+                break
             }
         }
     }, [open])
@@ -36,33 +35,38 @@ const AbilityTooltip = (props: any) => {
     // console.log(averageColor)
 
     return (
-        <Color src={props.img} crossOrigin="anonymous" format="hex">
-            {({ data, loading, error }) => {
-                return (
-                    <div className="tooltip" id="ability-tooltip" style={{ background: `radial-gradient(circle at top left, ${data} 0%, #182127 160px` }}>
-                        <div className="tooltip-line-one">
-                            <div className="tooltip-title">
-                                <img className="tooltip-img" alt={props.img} src={props.img} width="55px"></img>
-                                <h3>{ability.name_loc}</h3>
-                            </div>
-                        </div>
-                        <div className="tooltip-content">
-                            <AbilityAttributes ability={ability} />
-                            {ability['desc_loc'] && ability['desc_loc'].length &&
-                                <div className="tooltip-description" style={{ color: '#c9d1dd' }}>
-                                    <p dangerouslySetInnerHTML={{ __html: highlight_numbers(ability['desc_loc']) }}></p>
+        <>
+            {ability &&
+                <Color src={props.img} crossOrigin="anonymous" format="hex">
+                    {({ data, loading, error }) => {
+                        return (
+                            <div className="tooltip" id="ability-tooltip" style={{ background: `radial-gradient(circle at top left, ${data} 0%, #182127 160px` }}>
+                                <div className="tooltip-line-one">
+                                    <div className="tooltip-title">
+                                        <img className="tooltip-img" alt={props.img} src={props.img} width="55px"></img>
+                                        <h3>{ability.name_loc}</h3>
+                                    </div>
                                 </div>
-                            }
-                            <TooltipAttributes itemProperties={ability}></TooltipAttributes>
-                            <TooltipLore itemProperties={ability}></TooltipLore>
-                        </div>
-                        <div className="tooltip-footer">
-                            <CdMc mana_costs={ability.mana_costs} cooldowns={ability.cooldowns}></CdMc>
-                        </div>
-                    </div >
-                )
-            }}
-        </Color >
+                                <div className="tooltip-content">
+                                    <AbilityAttributes ability={ability} />
+                                    {ability['desc_loc'] && ability['desc_loc'].length &&
+                                        <div className="tooltip-description" style={{ color: '#c9d1dd' }}>
+                                            <p dangerouslySetInnerHTML={{ __html: highlight_numbers(ability['desc_loc']) }}></p>
+                                        </div>
+                                    }
+                                    <TooltipAttributes itemProperties={ability}></TooltipAttributes>
+                                    <TooltipLore itemProperties={ability}></TooltipLore>
+                                </div>
+                                <div className="tooltip-footer">
+                                    <CdMc mana_costs={ability.mana_costs} cooldowns={ability.cooldowns}></CdMc>
+                                </div>
+                            </div >
+                        )
+                    }}
+                </Color >
+            }
+        </>
     )
 }
+
 export default AbilityTooltip
