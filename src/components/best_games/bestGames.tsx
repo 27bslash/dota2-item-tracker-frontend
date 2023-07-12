70d9cb6fc82da4cb04cf83ea200c94823d373c35
27bslash <greenpear68@yahoo.co.uk>
2023-07-12 17:07:19 +0100

    best games component restructured

--- src/components/best_games/bestGames.tsx
+++ src/components/best_games/bestGames.tsx
@@ -2,9 +2,9 @@ import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import stringSearch from '../table/table_search/string_search';
 import blurred from '../../images/blurred-best-games.jpg'
 import Match from '../types/matchData';
+import { theme } from '../..';
 
 interface BenchmarksProps {
     benchmarks: {},
@@ -46,6 +46,7 @@ const BestGames = (props: { totalMatchData: Match[]; matchData: Match[]; updateR
         const sortingArr = ['player', '', 'gold_per_min', 'xp_per_min',
             'kills_per_min', 'last_hits_per_min', 'hero_damage_per_min', 'hero_healing_per_min', 'tower_damage', 'stuns_per_min', 'lhten']
         if (filtered.length) {
+            // sort benchmarks into correct order based on sortingArr
             const sortedBenchmarks = Object.keys(filtered[0]['benchmarks']).sort((a: any, b: any) => {
                 return sortingArr.indexOf(a) - sortingArr.indexOf(b)
             })
@@ -53,80 +54,89 @@ const BestGames = (props: { totalMatchData: Match[]; matchData: Match[]; updateR
         }
     }
     return (
-        <>
-            {loading &&
-                <img src={blurred} alt='blurred benchmarks' />
-            }
-            {!!bestgames.length && !loading &&
-                <div className="best-games-wrapper">
-                    <div className="best-games">
-                        <table>
-                            <thead>
-                                <tr>
-                                    {benchmarkKeys.map((x: any, i: number) => {
-                                        let header = x.split('_').map((char: string) => char[0]).join('')
-                                        if (i < 3) {
-                                            header = x
-                                        } else if (header.length === 4) {
-                                            header = header.replace('p', '')
-                                        } else if (x === 'lhten') {
-                                            header = "LH@10"
-                                        }
-                                        header = header.toUpperCase()
-                                        return <th key={i}>{header}</th>
-                                    })}
-                                </tr>
-                            </thead>
-                            <tbody className="best-games-body">
-                                {bestgames.map((match: any, i: number) => {
-                                    return (
-                                        <tr className="best-games-row" key={i}>
-                                            <td className="benchmark-cell">
-                                                <a className="player-name" href={`/player/${match['name']}`}>{match['name'].replace(/\(smurf.*\)/, '')}</a>
-                                            </td>
-                                            <td className="benchmark-cell ">
-                                                <FontAwesomeIcon className='copy-match-id' icon={faCopy} color='white' onClick={() => navigator.clipboard.writeText(match.id)} />
-                                            </td>
-                                            <td className="benchmark-cell">
-                                                <div className='svg-icon' id={match.role ? match.role.replace(' ', '-') : ''}
-                                                    onClick={() => props.updateRole(match.role)}></div>
-                                            </td>
-                                            {benchmarkKeys.map((k: any, idx: number) => {
-                                                if (k in match.benchmarks) {
-                                                    let pct = match.benchmarks[k]['pct']
-                                                    let raw = match.benchmarks[k]['raw']
-                                                    if (typeof pct == 'number') {
-                                                        pct = (pct * 100).toFixed(2)
-                                                        raw = raw.toFixed(2)
-                                                    }
-                                                    let color = '#EC494B'
-                                                    if (pct >= 80) color = '#5AA563'
-                                                    else if (pct >= 60) color = '#5499D2'
-                                                    else if (pct >= 40) color = '#C9AF1D'
-                                                    else if (pct >= 25) color = '#D89740'
-                                                    return (
-                                                        <td key={idx} className='benchmark-cell' style={{ textAlign: 'center' }}>
-                                                            <p>
-                                                                {pct &&
-                                                                    <>
-                                                                        <span className='benchmark-pct' style={{ color: color }}>{String(pct).replace(/\.00+/, '')}% </span>
-                                                                        <span className='benchmark-raw'>{String(raw).replace(/\.00+/, '')}</span>
-                                                                    </>
-                                                                }
-                                                            </p>
-                                                        </td>
-                                                    )
-                                                }
-                                            })}
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            }
-        </>
+        loading ? (
+            <img src={blurred} alt='blurred benchmarks' />
+        ) : (
+            <div className="best-games" style={{ 'width': '1200px', 'maxHeight': '140px', height: 'fit-content' }}>
+                <table>
+                    <BestGamesTableHeader benchmarkKeys={benchmarkKeys}></BestGamesTableHeader>
+                    <BestGamesTableBody updateRole={props.updateRole} benchmarkKeys={benchmarkKeys} bestgames={bestgames}></BestGamesTableBody>
+                </table>
+            </div>
+        )
     )
 }
+const BestGamesTableHeader = (props: { benchmarkKeys: string[] }) => {
+    //'linear-gradient(340deg,rgb(54, 59, 61) 0%,rgb(58, 94, 116) 100%)'
+    const gradString = `linear-gradient(340deg,${theme.palette.table.main} 0%,${theme.palette.primary.main} 100%)`
+    return (
+        <thead style={{
+            background: gradString
+        }}>
+            <tr>
+                {props.benchmarkKeys.map((benchmarkKey: any, i: number) => {
+                    let header = benchmarkKey.split('_').map((char: string) => char[0]).join('')
+                    if (i < 3) {
+                        header = benchmarkKey
+                    } else if (header.length === 4) {
+                        header = header.replace('p', '')
+                    } else if (benchmarkKey === 'lhten') {
+                        header = "LH@10"
+                    }
+                    header = header.toUpperCase()
+                    return <th key={i}>{header}</th>
+                })}
+            </tr>
+        </thead>
+    )
+}
+const BestGamesTableBody = (props: { updateRole: any; bestgames: Match[]; benchmarkKeys: any; }) => {
+    const { bestgames, benchmarkKeys } = props
+    return (
+        <tbody className="best-games-body">
+            {bestgames.map((match: any, i: number) => {
+                return (
+                    <tr className="best-games-row" key={i} style={{ backgroundColor: i % 2 === 0 ? theme.palette.table.main : theme.palette.table.secondary }} >
+                        <td className="benchmark-cell">
+                            <a className="player-name" href={`/player/${match['name']}`}>{match['name'].replace(/\(smurf.*\)/, '')}</a>
+                        </td>
+                        <td className="benchmark-cell ">
+                            <FontAwesomeIcon className='copy-match-id' icon={faCopy} color='white' onClick={() => navigator.clipboard.writeText(match.id)} />
+                        </td>
+                        <td className="benchmark-cell">
+                            <div className='svg-icon' id={match.role ? match.role.replace(' ', '-') : ''}
+                                onClick={() => props.updateRole(match.role)}></div>
+                        </td>
+                        {benchmarkKeys.map((k: any, idx: number) => {
+                            if (k in match.benchmarks) {
+                                let pct = match.benchmarks[k]['pct']
+                                let raw = match.benchmarks[k]['raw']
+                                if (typeof pct == 'number') {
+                                    pct = (pct * 100).toFixed(2)
+                                    raw = raw.toFixed(2)
+                                }
+                                let color = '#EC494B'
+                                if (pct >= 80) color = '#5AA563'
+                                else if (pct >= 60) color = '#5499D2'
+                                else if (pct >= 40) color = '#C9AF1D'
+                                else if (pct >= 25) color = '#D89740'
+                                return (
+                                    <td key={idx} className='benchmark-cell' style={{ textAlign: 'center', justifyContent: 'center' }}>
+                                        {pct &&
+                                            <>
+                                                <span className='benchmark-pct' style={{ color: color }}>{String(pct).replace(/\.00+/, '')}% </span>
+                                                <span className='benchmark-raw'>{String(raw).replace(/\.00+/, '')}</span>
+                                            </>
+                                        }
+                                    </td>
+                                )
+                            }
+                        })}
+                    </tr>
+                )
+            })}
+        </tbody>
+    )
+}
+
 export default BestGames
