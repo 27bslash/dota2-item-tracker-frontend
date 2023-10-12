9a164efaff62a7f5518d043b603ab5f2d1f90d5a
27bslash <greenpear68@yahoo.co.uk>
2023-10-12 17:02:59 +0100

    fixed tiers showing when no items present

--- src/components/HeroBuilds/itemBuild/neutralItems/neutralItems.tsx
+++ src/components/HeroBuilds/itemBuild/neutralItems/neutralItems.tsx
@@ -13,23 +13,24 @@ export const NeutralItems = (props: { data: NonProDataType[], itemData: Items })
                     <Typography align={'center'} variant='h5' color='white' >Neutral Items</Typography>
                     <Box alignItems={"center"} justifyContent={'center'} alignSelf={'center'} className="neutral-item-container flex">
                         {neutralItems.map((tierArr, i) => {
-                            // console.log(tierArr)
-                            return (
-                                <Box key={i} >
-                                    <Typography variant='h6' align={'center'} justifySelf={'center'} color='white' >Tier {i + 1}</Typography>
-                                    <Box padding={2} key={i} className="flex">
-                                        {tierArr.map((neutralItem, i) => {
-                                            const key: string = neutralItem[0]
-                                            const perc: number = neutralItem[1]['perc']
-                                            return (
-                                                <div key={i} className="neutral-item" >
-                                                    <ItemBuildImage k={key} itemData={props.itemData} perc={perc} data={props.data}></ItemBuildImage>
-                                                </div>
-                                            )
-                                        })}
+                            if (tierArr.length) {
+                                return (
+                                    <Box key={i} >
+                                        <Typography variant='h6' align={'center'} justifySelf={'center'} color='white' >Tier {i + 1}</Typography>
+                                        <Box padding={2} key={i} className="flex">
+                                            {tierArr.map((neutralItem, i) => {
+                                                const key: string = neutralItem[0]
+                                                const perc: number = neutralItem[1]['perc']
+                                                return (
+                                                    <div key={i} className="neutral-item" >
+                                                        <ItemBuildImage k={key} itemData={props.itemData} perc={perc} data={props.data}></ItemBuildImage>
+                                                    </div>
+                                                )
+                                            })}
+                                        </Box>
                                     </Box>
-                                </Box>
-                            )
+                                )
+                            }
                         })}
                     </Box>
                 </>
