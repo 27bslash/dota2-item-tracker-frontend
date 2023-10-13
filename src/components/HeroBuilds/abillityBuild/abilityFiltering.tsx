f065924d326e401110261fd16d44b6c3ef19bef7
27bslash <greenpear68@yahoo.co.uk>
2023-10-13 19:45:32 +0100

    changed slice amount

--- src/components/HeroBuilds/abillityBuild/abilityFiltering.tsx
+++ src/components/HeroBuilds/abillityBuild/abilityFiltering.tsx
@@ -1,12 +1,13 @@
+
 const abilityFilter = (data: any, ab = '') => {
     const abilities = []
     const aCount: { [key: string]: number } = {}
     for (let match of data) {
         if ('abilities' in match) {
-            const abilityArray = match['abilities'].filter((ability: any) => ability.type !== 'talent').map((x: any) => x.img).slice(0, 10)
+            const abilityArray = match['abilities'].filter((ability: any) => ability.type !== 'talent').map((x: any) => x.img).slice(0, 9)
             abilities.push(abilityArray)
             // console.log(match)
-            const key = match['abilities'].filter((ability: any) => ability.type !== 'talent').slice(0, 10).map((x: { img: string }) => x.img).join('__')
+            const key = match['abilities'].filter((ability: any) => ability.type !== 'talent').slice(0, 9).map((x: { img: string }) => x.img).join('__')
             aCount[key] = (aCount[key] + 1) || 1
         }
     }
