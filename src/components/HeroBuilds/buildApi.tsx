cbafd788bf506395cb2359f9ea1cb94e50cbfce9
27bslash <greenpear68@yahoo.co.uk>
2023-11-03 16:25:07 +0000

    conditional rendering of json

--- src/components/HeroBuilds/buildApi.tsx
+++ src/components/HeroBuilds/buildApi.tsx
@@ -6,13 +6,14 @@ import { fetchData } from "../../utils/fetchData"
 import { useEffect, useState } from 'react';
 import { baseApiUrl } from "../../App"
 import Items from "../types/Item"
+import { exists } from './../../utils/exists';
 
 export const BuildApi = () => {
     let t = useParams()
     const heroName = t['hero'] ? heroSwitcher(t['hero']) : ''
     const [totalPicks, setTotalPicks] = useState([])
     const [itemData, setItemData] = useState<Items>()
-
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         const fData = async () => {
             const matchDataUrl = 'https://0f2ezc19w3.execute-api.eu-west-2.amazonaws.com/dev/'
@@ -21,12 +22,16 @@ export const BuildApi = () => {
             setTotalPicks(matches['picks'])
             const itemData = await fetchData(`${baseApiUrl}files/items`)
             setItemData(itemData)
+            setLoading(false) // Set loading to false once data is fetched
+
         }
         fData()
     }, [heroName])
-
     const filteredData = useParseMatchData(false, undefined, heroName, { 'picks': totalPicks }, 0.19)
     const buildData = useHeroBuilds(filteredData, itemData!)
+    if (loading || !exists(buildData)) {
+        return null; // Render nothing
+    }
     return (
         <>
             {JSON.stringify(buildData)}
