d930ad5fdd7980f01933ca3068f769317520d136
27bslash <greenpear68@yahoo.co.uk>
2023-11-03 18:00:15 +0000

    build api works when not cached

--- src/components/HeroBuilds/buildApi.tsx
+++ src/components/HeroBuilds/buildApi.tsx
@@ -7,34 +7,40 @@ import { useEffect, useState } from 'react';
 import { baseApiUrl } from "../../App"
 import Items from "../types/Item"
 import { exists } from './../../utils/exists';
-
+const BuildDataCom = (props: any) => {
+    const filteredData = useParseMatchData(false, undefined, props.heroName, { 'picks': props.totalPicks }, 0.19)
+    const updatedBuildData = useHeroBuilds(filteredData, props.itemData!)
+    return (
+        updatedBuildData ? (
+            <div className="data">
+                {JSON.stringify(updatedBuildData)}
+            </div>
+        ) : (
+            <div className="nodata"></div>
+        )
+    )
+}
 export const BuildApi = () => {
     let t = useParams()
     const heroName = t['hero'] ? heroSwitcher(t['hero']) : ''
-    const [totalPicks, setTotalPicks] = useState([])
+    const [totalPicks, setTotalPicks] = useState<any>()
     const [itemData, setItemData] = useState<Items>()
-    const [loading, setLoading] = useState(false)
     useEffect(() => {
         const fData = async () => {
             const matchDataUrl = 'https://0f2ezc19w3.execute-api.eu-west-2.amazonaws.com/dev/'
             let url = `${matchDataUrl}hero/${heroName}/react-test?skip=0&length=10`
             const matches = await fetchData(url)
             setTotalPicks(matches['picks'])
-            const itemData = await fetchData(`${baseApiUrl}files/items`)
-            setItemData(itemData)
-            setLoading(false) // Set loading to false once data is fetched
-
+            const itemResponse = await fetchData(`${baseApiUrl}files/items`)
+            setItemData(itemResponse)
+            if (matches && itemData) {
+                console.log(matches, itemData)
+            }
         }
         fData()
-    }, [heroName])
-    const filteredData = useParseMatchData(false, undefined, heroName, { 'picks': totalPicks }, 0.19)
-    const buildData = useHeroBuilds(filteredData, itemData!)
-    if (loading || !exists(buildData)) {
-        return null; // Render nothing
-    }
+    }, [])
     return (
-        <>
-            {JSON.stringify(buildData)}
-        </>
+        totalPicks && heroName && itemData &&
+        <BuildDataCom itemData={itemData} totalPicks={totalPicks} heroName={heroName} ></BuildDataCom>
     )
 }
