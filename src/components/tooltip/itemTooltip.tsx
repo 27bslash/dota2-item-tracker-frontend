87b2f8dcc1ed9f76fa6c2b2ad10e4f3f4d49d8b1
27bslash <greenpear68@yahoo.co.uk>
2023-11-22 19:07:44 +0000

    itemProperties uses Item type

--- src/components/tooltip/itemTooltip.tsx
+++ src/components/tooltip/itemTooltip.tsx
@@ -4,62 +4,55 @@ import AghanimTooltip from "./aghanimTooltip";
 import TooltipAttributes from './tooltipAttributes';
 import TooltipDescription from './tooltipDescription';
 import TooltipLore from "./tooltipLore";
-interface Itemproperties {
-    id: number,
-    attrib: [],
-    displayName: string,
-    language?: { description?: string[], lore?: string[] },
-    name: string,
-    attributes: [{ name: string, value: string }],
-    stat: { cost: string, manaCost: string[], cooldown: string[] }
-    cost: number,
-    dname: string
-}
+import { PageHeroData } from "../types/heroData";
+import { Items, Item } from "../types/Item";
+import { useTableContext } from "../table/tableContext";
+
+
 interface ItemTooltipProps {
     itemId?: number,
-    items: any,
     itemKey: string,
     type: string,
-    heroData?: any[],
     img: string,
-    heroName?: string,
 }
 const ItemTooltip = (props: ItemTooltipProps) => {
-    const [open, setOpen] = useState(false)
-    const [itemProperties, setItemProperties] = useState<Itemproperties>()
+    const [itemProperties, setItemProperties] = useState<Item>()
+    const { items, row, heroData } = useTableContext()
     useEffect(() => {
-        if (props.items) {
-            setItemProperties(props.items.items[props.itemKey])
+        if (items) {
+            setItemProperties(items.items[props.itemKey])
         }
-    }, [open])
+    }, [])
     return (
         <>
-            {itemProperties && !props.heroData && (
-                <div className="tooltip">
-                    <div className="tooltip-line-one item-tooltip-line-one">
-                        <img className="tooltip-img" alt={props.img} src={props.img} width='75px' style={{ marginRight: '10px' }}></img>
-                        <div className="item-tooltip-title">
-                            <h3>{itemProperties.dname}</h3>
-                            {+itemProperties.cost > 0 &&
-                                <div className="cost-wrapper">
-                                    <img alt='gold' className="gold-img" src="https://steamcdn-a.akamaihd.net/apps/dota2/images/tooltips/gold.png" style={{ marginRight: '5px' }}></img>
-                                    <h4>{itemProperties.cost}</h4>
+            {itemProperties &&
+                <>
+                    {props.type !== 'scepter' && props.type !== 'shard' ? (
+                        <div className="tooltip">
+                            <div className="tooltip-line-one item-tooltip-line-one">
+                                <img className="tooltip-img" alt={props.img} src={props.img} width='75px' style={{ marginRight: '10px' }}></img>
+                                <div className="item-tooltip-title">
+                                    <h3>{itemProperties.dname}</h3>
+                                    {itemProperties.cost && itemProperties.cost > 0 &&
+                                        <div className="cost-wrapper">
+                                            <img alt='gold' className="gold-img" src="https://steamcdn-a.akamaihd.net/apps/dota2/images/tooltips/gold.png" style={{ marginRight: '5px' }}></img>
+                                            <h4>{itemProperties.cost}</h4>
+                                        </div>
+                                    }
                                 </div>
-                            }
-                        </div>
 
-                    </div>
-                    <div className="tooltip-content">
-                        <TooltipAttributes itemProperties={itemProperties}></TooltipAttributes>
-                        <TooltipDescription itemProperties={itemProperties}></TooltipDescription>
-                        <TooltipLore itemProperties={itemProperties}></TooltipLore>
-                    </div>
-                </div>
-            )}
-            {
-                props.heroData && props.heroName && (
-                    <AghanimTooltip heroName={props.heroName} heroData={props.heroData} type={props.type}></AghanimTooltip>
-                )
+                            </div>
+                            <div className="tooltip-content">
+                                <TooltipAttributes itemProperties={itemProperties}></TooltipAttributes>
+                                <TooltipDescription itemProperties={itemProperties}></TooltipDescription>
+                                <TooltipLore itemProperties={itemProperties}></TooltipLore>
+                            </div>
+                        </div>
+                    ) : (
+                        <AghanimTooltip heroName={row!.hero} heroData={heroData} type={props.type}></AghanimTooltip>
+                    )
+                    }
+                </>
             }
         </>
     )
