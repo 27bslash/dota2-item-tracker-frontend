8dc90b06608579c124a884c9125293a75bf9c441
27bslash <greenpear68@yahoo.co.uk>
2023-12-18 18:51:58 +0000

    fixed home page removing patch filters

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { useParams } from "react-router";
 import Hero from "../types/heroList";
 import HomeButton from "./nav_button"
 import NavSearch from './search';
@@ -11,9 +12,11 @@ interface NavProps {
 }
 
 const Nav = ({ highlightHero, filterHeroes, filteredByButton, heroList, playerList }: NavProps) => {
+    const params = useParams()
+    const patchString = params['patch'] ? params['patch'] : ''
     return (
         <div className="navbar">
-            <HomeButton text='HOME' link='/' />
+            <HomeButton text='HOME' link={`/${patchString}`} />
             {/* <HomeButton text='CHAPPIE' link='/chappie' /> */}
             <div className="search-container" style={{ width: '100%' }}>
                 <NavSearch highlightHero={highlightHero} filterHeroes={filterHeroes} filteredByButton={filteredByButton} heroList={heroList} playerList={playerList} />
