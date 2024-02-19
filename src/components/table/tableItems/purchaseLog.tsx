4d087c8780b22f2b532984a519c1a8df70c578b8
27bslash <greenpear68@yahoo.co.uk>
2024-02-19 14:27:37 +0000

    fixed additional units appearing on arc warden items

--- src/components/table/tableItems/purchaseLog.tsx
+++ src/components/table/tableItems/purchaseLog.tsx
@@ -1,34 +1,57 @@
-import { exists } from "../../../utils/exists";
-import { RoleStrings } from "../../home/home";
-import Items from "../../types/Item";
-import { PageHeroData } from "../../types/heroData";
-import DotaMatch from "../../types/matchData";
-import { useTableContext } from "../tableContext";
-import { FinalItems } from "./finalItems";
-import TableItem from "./tableItem";
-import { humanReadableTime } from "./tableItems";
+import { exists } from '../../../utils/exists'
+import { RoleStrings } from '../../home/home'
+import Items from '../../types/Item'
+import { PageHeroData } from '../../types/heroData'
+import DotaMatch from '../../types/matchData'
+import { useTableContext } from '../tableContext'
+import { FinalItems } from './finalItems'
+import TableItem from './tableItem'
+import { humanReadableTime } from './tableItems'
 
 type PurchaseLogProps = {
     tableItemProps: {
-        matchId: number;
-        overlay: boolean;
-        role: RoleStrings;
-        updateMatchData: (data: DotaMatch[]) => void;
+        matchId: number
+        overlay: boolean
+        role: RoleStrings
+        updateMatchData: (data: DotaMatch[]) => void
         heroName?: string
     }
 }
 export function PurchaseLog({ tableItemProps }: PurchaseLogProps) {
     const { showStarter, row } = useTableContext()
-    return (<div className="purchases">
-        {!showStarter ? <>
-            <FinalItems bear={!!exists(row.additional_units)} itemList={row.additional_units || []}></FinalItems>
-            <FinalItems itemList={row.final_items} heroName={tableItemProps.heroName}></FinalItems>
-        </> : row.starting_items && <div className="flex">
-            {row.starting_items.map((item, i: number) => {
-                const time = humanReadableTime(item['time']);
-                return <TableItem {...tableItemProps} time={time} key={i} itemKey={item.key} type='item' starter={true}>
-                </TableItem>;
-            })}
-        </div>}
-    </div>);
+    const exp = !!exists(row.additional_units) && row.hero == 'lone_druid'
+    return (
+        <div className="purchases">
+            {!showStarter ? (
+                <>
+                    <FinalItems
+                        bear={exp}
+                        itemList={exp ? row.additional_units! : []}
+                    ></FinalItems>
+                    <FinalItems
+                        itemList={row.final_items}
+                        heroName={tableItemProps.heroName}
+                    ></FinalItems>
+                </>
+            ) : (
+                row.starting_items && (
+                    <div className="flex">
+                        {row.starting_items.map((item, i: number) => {
+                            const time = humanReadableTime(item['time'])
+                            return (
+                                <TableItem
+                                    {...tableItemProps}
+                                    time={time}
+                                    key={i}
+                                    itemKey={item.key}
+                                    type="item"
+                                    starter={true}
+                                ></TableItem>
+                            )
+                        })}
+                    </div>
+                )
+            )}
+        </div>
+    )
 }
