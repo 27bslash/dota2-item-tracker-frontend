9e79284d22ae84849a4b622a798962ea5157be6a
27bslash <greenpear68@yahoo.co.uk>
2024-05-28 21:32:12 +0100

    added color to facet tooltip

--- src/components/tooltip/facetTooltip.tsx
+++ src/components/tooltip/facetTooltip.tsx
@@ -3,40 +3,87 @@ import { useState, useEffect } from 'react'
 import { usePageContext } from '../stat_page/pageContext'
 import { highlight_numbers } from './tooltipDescription'
 import { FacetObj } from '../types/heroData'
+import { Box, Typography } from '@mui/material'
 
 type facetToolipProps = {
     img: string
     facet: FacetObj
 }
 export const FacetTooltip = ({ img, facet }: facetToolipProps) => {
-    const [open, setOpen] = useState(false)
-
-    // const fac = new FastAverageColor()
-    // const options = { width: '55px', height: '55px' }
-
-    // console.log(image)
-    // const averageColor = fac.getColor(image)
-    // console.log(averageColor)
-    const colorMap = {
-        '4': '#926B96',
-        '3': '#7F9EA2',
-    }
-    const gradientMap = {}
-    //  linear-gradient(to right, #9F3C3C, #4A2040);
-    console.log(facet)
+    const colorMap = [
+        [
+            'linear-gradient(to right, #9F3C3C, #4A2040)',
+            'linear-gradient(to right, #954533, #452732)',
+            'linear-gradient(to right, #A3735E, #4F2A25)',
+        ],
+        [
+            'linear-gradient(to right, #C8A45C, #6F3D21)',
+            'linear-gradient(to right, #C6A158, #604928)',
+            'linear-gradient(to right, #CAC194, #433828)',
+            'linear-gradient(to right, #C3A99A, #4D352B)',
+        ],
+        [
+            'linear-gradient(to right, #A2B23E, #2D5A18)',
+            'linear-gradient(to right, #7EC2B2, #29493A)',
+            'linear-gradient(to right, #538564, #1C3D3F)',
+            'linear-gradient(to right, #9A9F6A, #223824)',
+            'linear-gradient(to right, #9FAD8E, #3F4129)',
+        ],
+        [
+            'linear-gradient(to right, #727CB2, #342D5B)',
+            'linear-gradient(to right, #547EA6, #2A385E)',
+            'linear-gradient(to right, #6BAEBC, #135459)',
+            'linear-gradient(to right, #94B5BA, #385B59)',
+        ],
+        [
+            'linear-gradient(to right, #B57789, #412755)',
+            'linear-gradient(to right, #9C70A4, #282752)',
+            'linear-gradient(to right, #675CAE, #261C44)',
+        ],
+        [
+            'linear-gradient(to right, #565C61, #1B1B21)',
+            'linear-gradient(to right, #6A6D73, #29272C)',
+            'linear-gradient(to right, #95A9B1, #3E464F)',
+            'linear-gradient(to right, #ADB6BE, #4E5557)',
+        ],
+    ]
     return (
         <>
             {facet && (
                 <div
+                    style={{ width: '400px' }}
                     className="tooltip"
                     id="facet-tooltip"
-                    // style={{
-                    //     background: `radial-gradient(circle at top left, ${data} 0%, #11171c 160px`,
-                    // }}
                 >
-                    <div className="tooltip-line-one">
-                        <div className="tooltip-title">
-                            <div>
+                    <div
+                        className="bg"
+                        style={{
+                            background:
+                                colorMap[facet.color][facet.gradient_id],
+                        }}
+                    >
+                        <div
+                            className="tooltip-line-one"
+                            style={{
+                                backgroundImage:
+                                    'url("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/facets/ripple_texture.png")',
+                                backgroundSize: 'cover',
+                                padding: '0px 10px 0px 0px',
+                                height: '70px',
+                            }}
+                        >
+                            <Box
+                                alignItems={'center'}
+                                justifyContent={'center'}
+                                justifyItems={'center'}
+                                sx={{
+                                    height: '100%',
+                                    width: '20%',
+                                    background:
+                                        ' linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0))',
+                                    display: 'flex',
+                                }}
+                            >
                                 <img
                                     style={{
                                         filter: 'drop-shadow(0px 3px 2px rgba(0, 0, 0, 0.3))',
@@ -44,13 +91,28 @@ export const FacetTooltip = ({ img, facet }: facetToolipProps) => {
                                     className="tooltip-img"
                                     alt={img}
                                     src={img}
-                                    width="55px"
+                                    width="35px"
                                 ></img>
+                            </Box>
+                            <div className="tooltip-title">
+                                <div style={{}}></div>
+                                <Typography
+                                    variant="h3"
+                                    sx={{
+                                        fontSize: '22px !important',
+                                        marginLeft: 'auto',
+                                        textTransform: 'uppercase',
+                                    }}
+                                >
+                                    {facet.title_loc}
+                                </Typography>
                             </div>
-                            <h3>{facet.title_loc}</h3>
                         </div>
                     </div>
-                    <div className="tooltip-content">
+                    <div
+                        className="tooltip-content"
+                        style={{ backgroundColor: '#181f24' }}
+                    >
                         {facet['description_loc'] &&
                             facet['description_loc'].length && (
                                 <div
