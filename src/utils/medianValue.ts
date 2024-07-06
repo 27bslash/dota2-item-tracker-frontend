319d9d504bfb5e50f8de056317b8a7f5da1bc925
27bslash <greenpear68@yahoo.co.uk>
2024-07-06 19:31:17 +0100

    fixed median value sorting

--- src/utils/medianValue.ts
+++ src/utils/medianValue.ts
@@ -1,5 +1,5 @@
 export const medianValue = (values: number[]) => {
-    values.sort()
+    values.sort((a,b) => a-b)
     const half = Math.floor(values.length / 2);
     return values.length % 2 ? values[half] : (values[half - 1] + values[half]) / 2.0;
 }
