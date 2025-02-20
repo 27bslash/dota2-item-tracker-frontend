8be9e0e8f74bc1c214f6732addfd2d810938ca3a
27bslash <greenpear68@yahoo.co.uk>
2025-02-20 18:20:14 +0000

    deprecated checks for old facets in hero builds

--- src/components/HeroBuilds/facets/FacetBuild.tsx
+++ src/components/HeroBuilds/facets/FacetBuild.tsx
@@ -1,73 +1,77 @@
-import { useEffect, useState } from 'react'
-import { usePageContext } from '../../stat_page/pageContext'
-import { FacetObj } from '../../types/heroData'
-import { FacetContent } from './facetContent'
-type FacetProps = {
-    data: {
-        key: number
-        count: number
-        perc: string
-        title?: string
-    }[]
-}
-export const FacetBuild = ({ data }: FacetProps) => {
-    const { heroData, nameParam } = usePageContext()
-    const [facets, setFacets] = useState<FacetObj[]>()
-    useEffect(() => {
-        setFacets(heroData[nameParam]['facets'])
-    }, [heroData, nameParam])
-    const sortedData = data.sort((a, b) => {
-        return b['count'] - a['count']
-    })
-    const mappedData = sortedData.map((x) => {
-        if (!facets) return x
-        if (!facets[x['key'] - 1]['Deprecated']) {
-            x.title = facets[x['key'] - 1].title_loc.toLowerCase()
-            return x
-        }
-        x.title = facets[facets?.length - 1].title_loc.toLowerCase()
-        return x
-    })
-    const mergedData = Object.values(
-        mappedData.reduce((acc: any, item) => {
-            if (!item.title) return acc
-            if (!acc[item.title]) {
-                acc[item.title] = { ...item }
-            } else {
-                acc[item.title].count += item.count
-            }
-            return acc
-        }, {})
-    )
+import { useEffect, useState, useMemo } from "react";
+import { usePageContext } from "../../stat_page/pageContext";
+import { FacetObj } from "../../types/heroData";
+import { FacetContent } from "./facetContent";
 
-    const maps = []
-    for (let i = 0; i < mergedData.length; i += 2) {
-        maps.push([sortedData[i], sortedData[i + 1]])
+type FacetProps = {
+  data: {
+    key: number;
+    count: number;
+    perc: string;
+    title: string;
+  }[];
+};
+
+export const FacetBuild = ({ data }: FacetProps) => {
+  const { heroData, nameParam } = usePageContext();
+  const [facets, setFacets] = useState<FacetObj[] | null>(null);
+
+  useEffect(() => {
+    if (heroData[nameParam]?.facets) {
+      setFacets(heroData[nameParam]["facets"]);
     }
-    return (
-        <div className={`facets ${data.length <= 2 ? 'flex' : ''}`}>
-            {facets &&
-                maps.map((arr, i: number) => {
-                    return (
-                        <div
-                            className="facets-group"
-                            style={{ display: 'flex' }}
-                            key={i}
-                        >
-                            {arr.map((x, j) => {
-                                if (x) {
-                                    return (
-                                        <FacetContent
-                                            key={j}
-                                            facetStats={x}
-                                            facets={facets}
-                                        ></FacetContent>
-                                    )
-                                }
-                            })}
-                        </div>
-                    )
-                })}
-        </div>
-    )
-}
+  }, [heroData, nameParam]);
+
+  const sortedData = useMemo(() => {
+    return [...data].sort((a, b) => b.count - a.count);
+  }, [data]);
+
+  const mappedData = useMemo(() => {
+    return sortedData.map((x) => {
+      if (!facets) return x;
+      if (!facets[x["key"] - 1]["Deprecated"]) {
+        x.title = facets[x["key"] - 1].title_loc.toLowerCase();
+        return x;
+      }
+      x.title = facets[facets?.length - 1].title_loc.toLowerCase();
+      return x;
+    });
+  }, [sortedData, facets]);
+
+  const mergedData = useMemo(() => {
+    console.log(mappedData);
+    return Object.values(
+      mappedData.reduce((acc: Record<string, any>, item) => {
+        if (!item.title) return acc;
+        if (!acc[item.title]) {
+          acc[item.title] = { ...item };
+        } else {
+          acc[item.title].count += item.count;
+        }
+        return acc;
+      }, {})
+    );
+  }, [mappedData]);
+
+  const maps = useMemo(() => {
+    const grouped = [];
+    for (let i = 0; i < mergedData.length; i += 2) {
+      grouped.push([mergedData[i], mergedData[i + 1]]);
+    }
+    console.log(grouped);
+    return grouped;
+  }, [mergedData]);
+
+  return (
+    <div className={`facets ${data.length <= 2 ? "flex" : ""}`}>
+      {facets &&
+        maps.map((arr, i) => (
+          <div className="facets-group" style={{ display: "flex" }} key={i}>
+            {arr.map((x, j) =>
+              x ? <FacetContent key={j} facetStats={x} facets={facets} /> : null
+            )}
+          </div>
+        ))}
+    </div>
+  );
+};
--- src/components/HeroBuilds/facets/facetContent.tsx
+++ src/components/HeroBuilds/facets/facetContent.tsx
@@ -1,121 +1,116 @@
-import { Box, Typography } from '@mui/material'
-import { grey } from '@mui/material/colors'
-import { cleanDecimal } from '../../../utils/cleanDecimal'
-import colourWins from '../../../utils/colourWins'
-import { usePageContext } from '../../stat_page/pageContext'
-import { facetBackground, FacetTooltip } from '../../tooltip/facetTooltip'
-import Tip from '../../tooltip/tooltip'
-import { FacetObj } from '../../types/heroData'
+import { Box, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { cleanDecimal } from "../../../utils/cleanDecimal";
+import colourWins from "../../../utils/colourWins";
+import { usePageContext } from "../../stat_page/pageContext";
+import { facetBackground, FacetTooltip } from "../../tooltip/facetTooltip";
+import Tip from "../../tooltip/tooltip";
+import { FacetObj } from "../../types/heroData";
 type FacetContentProps = {
-    facets: FacetObj[]
-    facetStats: { key: number; count: number; perc: string }
-}
+  facets: FacetObj[];
+  facetStats: { key: number; count: number; perc: string; title: string };
+};
 
 export const FacetContent = ({ facets, facetStats }: FacetContentProps) => {
-    const { updateSearchResults, heroData, nameParam } = usePageContext()
-    let decrement = 1
-    let facet: any
-    for (const f of facets) {
-        if (!facets[facetStats['key'] - decrement]) {
-            decrement += 1
-            continue
-        }
-        facet = facets[facetStats['key'] - decrement]
-        break
+  const { updateSearchResults, heroData, nameParam } = usePageContext();
+  let decrement = 1;
+  let facet: any;
+  for (const f of facets) {
+    if (
+      !f.Deprecated &&
+      facetStats.title.toLowerCase() === f.title_loc.toLowerCase()
+    ) {
+      facet = f;
+      break;
     }
-    const icon = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/facets/${facet.icon}.png`
-    const { background, filter } = facetBackground(facet)
-    return (
+  }
+  if (!facet) return null;
+  const icon = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/facets/${facet.icon}.png`;
+  const { background, filter } = facetBackground(facet);
+  return (
+    <div
+      onClick={() =>
+        updateSearchResults(
+          facetStats["key"],
+          "facet",
+          "variant",
+          facet.title_loc
+        )
+      }
+    >
+      <Tip
+        placement={"left-end"}
+        component={
+          <FacetTooltip
+            img={icon}
+            facet={facet}
+            heroStats={heroData[nameParam]}
+          />
+        }
+      >
         <div
-            onClick={() =>
-                updateSearchResults(
-                    facetStats['key'],
-                    'facet',
-                    'variant',
-                    facet.title_loc
-                )
-            }
+          style={{
+            background: background,
+            display: "flex",
+            alignItems: "center",
+            height: "fit-content",
+            padding: "5px",
+            width: "300px",
+          }}
         >
-            <Tip
-                placement={'left-end'}
-                component={
-                    <FacetTooltip
-                        img={icon}
-                        facet={facet}
-                        heroStats={heroData[nameParam]}
-                    />
-                }
+          <img
+            height={30}
+            src={icon}
+            style={{
+              filter: "drop-shadow(0px 3px 2px rgba(0, 0, 0, 0.3))",
+            }}
+          ></img>
+          <Box paddingLeft={2} paddingRight={2}>
+            {facetPicks(facetStats)}
+            <Typography
+              letterSpacing={1.3}
+              fontFamily="Reaver"
+              textTransform="uppercase"
+              fontWeight="bold"
             >
-                <div
-                    style={{
-                        background: background,
-                        display: 'flex',
-                        alignItems: 'center',
-                        height: 'fit-content',
-                        padding: '5px',
-                        width: '300px',
-                    }}
-                >
-                    <img
-                        height={30}
-                        src={icon}
-                        style={{
-                            filter: 'drop-shadow(0px 3px 2px rgba(0, 0, 0, 0.3))',
-                        }}
-                    ></img>
-                    <Box paddingLeft={2} paddingRight={2}>
-                        {facetPicks(facetStats)}
-                        <Typography
-                            letterSpacing={1.3}
-                            fontFamily="Reaver"
-                            textTransform="uppercase"
-                            fontWeight="bold"
-                        >
-                            {facet.title_loc}
-                        </Typography>
-                    </Box>
-                </div>
-            </Tip>
+              {facet.title_loc}
+            </Typography>
+          </Box>
         </div>
-    )
-}
+      </Tip>
+    </div>
+  );
+};
 function facetPicks(x: { key: number; count: number; perc: string }) {
-    return (
-        <div className="flex">
-            <Typography
-                color={grey['400']}
-                marginRight={1}
-                letterSpacing={1}
-                fontSize={'0.9rem'}
-            >
-                Picks:
-                <span
-                    style={{
-                        marginLeft: '5px',
-                        color: 'orange',
-                    }}
-                >
-                    {x['count']}
-                </span>
-            </Typography>
-            <Typography
-                color={grey['400']}
-                letterSpacing={1}
-                fontSize={'0.9rem'}
-            >
-                Pick Rate:
-                <span
-                    style={{
-                        marginLeft: '5px',
-                        color:
-                            +x['perc'] > 50
-                                ? colourWins(x['perc'])
-                                : grey['200'],
-                    }}
-                >
-                    {cleanDecimal(x['perc'])}%
-                </span>
-            </Typography>
-        </div>
-    )
+  return (
+    <div className="flex">
+      <Typography
+        color={grey["400"]}
+        marginRight={1}
+        letterSpacing={1}
+        fontSize={"0.9rem"}
+      >
+        Picks:
+        <span
+          style={{
+            marginLeft: "5px",
+            color: "orange",
+          }}
+        >
+          {x["count"]}
+        </span>
+      </Typography>
+      <Typography color={grey["400"]} letterSpacing={1} fontSize={"0.9rem"}>
+        Pick Rate:
+        <span
+          style={{
+            marginLeft: "5px",
+            color: +x["perc"] > 50 ? colourWins(x["perc"]) : grey["200"],
+          }}
+        >
+          {cleanDecimal(x["perc"])}%
+        </span>
+      </Typography>
+    </div>
+  );
 }
