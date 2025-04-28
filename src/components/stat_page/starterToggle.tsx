c9f1fbbda6e91484557235eb570bc3ad8cc74fc0
27bslash <greenpear68@yahoo.co.uk>
2025-04-29 00:00:35 +0100

    pro filter button does not show on player pages

--- src/components/stat_page/starterToggle.tsx
+++ src/components/stat_page/starterToggle.tsx
@@ -1,39 +1,56 @@
-import { FormControlLabel, styled, Switch, SwitchProps } from '@mui/material';
+import {
+  FormControlLabel,
+  styled,
+  Switch,
+  SwitchProps,
+  Typography,
+} from "@mui/material";
 import { theme } from "../../main";
 
 const ToggleSwitch = styled((props: SwitchProps) => (
-    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
 ))(({ theme }) => ({
-    '& .MuiSwitch-switchBase': {
-        '&.Mui-checked': {
-            // transform: 'translateX(16px)',
-            color: '#fff',
-            '& + .MuiSwitch-track': {
-                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : 'secondary',
-                opacity: 1,
-
-            },
-            '&.Mui-disabled + .MuiSwitch-track': {
-                backgroundColor: 'red',
-            },
-        },
-    },
-    '& .MuiSwitch-track': {
-        backgroundColor: '#606060',
+  "& .MuiSwitch-switchBase": {
+    "&.Mui-checked": {
+      // transform: 'translateX(16px)',
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor:
+          theme.palette.mode === "dark" ? "#2ECA45" : "secondary",
         opacity: 1,
-    }
-}))
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        backgroundColor: "red",
+      },
+    },
+  },
+  "& .MuiSwitch-track": {
+    backgroundColor: "#606060",
+    opacity: 1,
+  },
+}));
 
-export const StarterToggle = ({ updateStarter }: { updateStarter: () => void }) => {
-    return (
-        <div className="starter-toggle">
-            <FormControlLabel
-                value="start"
-                control={<ToggleSwitch color="success" onChange={updateStarter} />}
-                label="Starting Items"
-                labelPlacement="start"
-                sx={{ padding: '4px 0px 4px 8px', backgroundColor: theme.palette.primary.main, borderRadius: '5px', border: ' solid 2px black', margin: '0', marginRight: 'auto', }}
-            />
-        </div>
-    )
-}
+export const StarterToggle = ({
+  updateStarter,
+}: {
+  updateStarter: () => void;
+}) => {
+  return (
+    <div className="starter-toggle">
+      <FormControlLabel
+        value="start"
+        control={<ToggleSwitch color="success" onChange={updateStarter} />}
+        label={<Typography>Starting Items</Typography>}
+        labelPlacement="start"
+        sx={{
+          padding: "4px 0px 4px 8px",
+          backgroundColor: theme.palette.primary.main,
+          borderRadius: "5px",
+          border: " solid 2px black",
+          margin: "0",
+          marginRight: "auto",
+        }}
+      />
+    </div>
+  );
+};
