4a5c081d0cca10d7e27ad13d72a6590162a710ac
27bslash <greenpear68@yahoo.co.uk>
2026-02-17 13:16:38 +0000

    added 500 error retries

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,21 +1,40 @@
 import { baseApiUrl } from "./../App";
 export const fetchData = async (url: string, retries = 2, delay = 1000) => {
   const newUrl = url.replace(/(\?.*?)\?(.*)/gm, "$1&$2");
-  for (let i = 0; i < retries; i++) {
-    let response = await fetch(newUrl);
-    if (response.status === 200) return await response.json();
-    if (response.status === 304) {
-      response = await fetch(newUrl.replace(/&time=\d*/, ""));
-      return await response.json();
-    } else if (response.status === 429) {
-      const retryAfter = response.headers.get("retry-after");
-      const waitTime = retryAfter
-        ? parseInt(retryAfter, 10) * 1000
-        : delay * (i + 1);
-      console.log(
-        `Rate limited. ${url} Retrying in ${waitTime / 1000} seconds...`
-      );
-      await new Promise((res) => setTimeout(res, waitTime));
+  const wait = (ms: number) =>
+    new Promise((res) => {
+      setTimeout(res, ms);
+    });
+
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      let response = await fetch(newUrl);
+      if (response.status === 200) return await response.json();
+      if (response.status === 304) {
+        response = await fetch(newUrl.replace(/&time=\d*/, ""));
+        return await response.json();
+      }
+
+      if (response.status === 429) {
+        const retryAfter = response.headers.get("retry-after");
+        const waitTime = retryAfter
+          ? parseInt(retryAfter, 10) * 1000
+          : delay * attempt;
+        console.log(
+          `Rate limited. ${url} Retrying in ${waitTime / 1000} seconds...`
+        );
+        await wait(waitTime);
+        continue;
+      }
+
+      if (attempt === retries) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+
+      await wait(delay * attempt);
+    } catch (error) {
+      if (attempt === retries) throw error;
+      await wait(delay * attempt);
     }
   }
 };
