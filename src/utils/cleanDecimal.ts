204a4dca253da7b7aac0075961792dadee64b20a
27bslash <greenpear68@yahoo.co.uk>
2026-04-13 23:26:50 +0100

    changed clean decimal to have variable float sizes

--- src/utils/cleanDecimal.ts
+++ src/utils/cleanDecimal.ts
@@ -1,9 +1,9 @@
-export const cleanDecimal = (decimal: string | number) => {
-    if (typeof (decimal) === 'string') {
-        return String(parseFloat(decimal).toFixed(2)).replace(/\.00/, '')
-    } else if (typeof (decimal) === 'number') {
-        return String((decimal).toFixed(2)).replace(/\.00/, '')
-    } else {
-        return '0'
-    }
-}
+export const cleanDecimal = (decimal: string | number, round = 2) => {
+  if (typeof decimal === "string") {
+    return String(parseFloat(decimal).toFixed(round)).replace(/\.00/, "");
+  } else if (typeof decimal === "number") {
+    return String(decimal.toFixed(round)).replace(/\.00/, "");
+  } else {
+    return "0";
+  }
+};
